9ba9e8a4412578bc466394870d057e597aacf835
Brittany <batherton@collegeforcreativestudies.edu>
2016-11-02 15:55:20 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,25 +6,22 @@ $(document).ready(function() {
     success: function(data) {
 
       $.each(data, function(i, person) {
-
-
-
         var ranLeft = Math.floor($(window).width('max-width', '300px') * Math.random());
         var ranTop = Math.floor($(window).width() * Math.random());
-        $('#message').prepend('<div style="margin-left:' + ranLeft + 'px;font-size:' + (3 * person.stickeramount) + 'px" class="person"' + '" data-sex="' + person.sex + '">' + person.name + '</div>');
+        var randomRotationClass = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
-      });
-		
-    	$(".person").each(function(){
-      	if ($(this).data("sex") == "Female") {
-        	$(this).addClass("pink");
+        $('#message').prepend('<div style="margin-left:' + ranLeft + 'px;font-size:' + (3 * person.stickeramount) + 'px" class="person rot-' + randomRotationClass + '"' + '" data-sex="' + person.sex + '">' + person.name + '</div>');
+});
+
+      $(".person").each(function() {
+        if ($(this).data("sex") == "Female") {
+          $(this).addClass("pink");
         } else {
-        	$(this).addClass("blue");
+          $(this).addClass("blue");
         };
       });
-      $(".person").click(function() {
-        alert("The person was clicked.");
-      });
+     
+      
     }
   })
 });
